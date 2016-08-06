220b621ad16340d6b03a13c569a5ac824ed355f3
John Pettigrew <john.pettigrew404@gmail.com>
2016-08-06 12:29:38 -0500

    use command line arguments to make app more generic

--- customizer.go
+++ customizer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	"image/jpeg"
@@ -14,42 +15,73 @@ import (
 
 func main() {
 
-	//Check parameters
-	if len(os.Args) < 4 {
-		fmt.Println("Usage: image-customizer [dst] [mask] [output]")
-		return
+	var flipImage, invertColors bool
+	var outputPath, inputPath, maskPath, maskDrawPath string
+
+	//cmd line arguments
+	flag.BoolVar(&flipImage, "f", false, "Flip the image that will be drawn.")
+	flag.BoolVar(&invertColors, "i", false, "Invert the colors for image that will be drawn.")
+	flag.StringVar(&outputPath, "out", "", "Output file path (required)")
+	flag.StringVar(&inputPath, "in", "", "Input file path (required)")
+	flag.StringVar(&maskPath, "m", "", "Mask file path")
+	flag.StringVar(&maskDrawPath, "mi", "", "File path for an image to draw using a supplied mask")
+	flag.Parse()
+
+	//validation
+	if outputPath == "" {
+		errorAndExit("Output file is required\n")
 	}
 
-	dstPath := os.Args[1]
-	maskPath := os.Args[2]
-	outputPath := os.Args[3]
+	if inputPath == "" {
+		errorAndExit("Input file is required\n")
+	}
 
-	//Read in images
-	dst, err := readImage(dstPath)
+	if maskPath != "" || maskDrawPath != "" {
+		if maskPath == "" || maskDrawPath == "" {
+			errorAndExit("Mask path and mask image to draw path are both required to draw mask\n")
+		}
+	}
+
+	//Read in input image
+	dst, err := readImage(inputPath)
 	if err != nil {
-		log.Fatal("Error reading dst")
+		errorAndExit("Error reading input image\n")
 	}
 
-	mask, err := readImage(maskPath)
-	if err != nil {
-		log.Fatal("Error reading src")
+	//apply transforms
+	if flipImage {
+		dst = imgtransforms.Flip(dst)
 	}
 
-	//Scale mask
-	finalMask := image.NewRGBA(dst.Bounds())
-	draw.ApproxBiLinear.Scale(finalMask, dst.Bounds(), mask, mask.Bounds(), draw.Over, nil)
-
-	//Create changed dst
-	changedDst := imgtransforms.Flip(dst)
-	changedDst = imgtransforms.InvertColors(changedDst)
+	if invertColors {
+		dst = imgtransforms.InvertColors(dst)
+	}
 
 	//Convert dst
 	dstB := dst.Bounds()
 	finalDst := image.NewRGBA(dstB)
 	draw.Draw(finalDst, finalDst.Bounds(), dst, dstB.Min, draw.Src)
 
-	//Draw our image
-	draw.DrawMask(finalDst, finalDst.Bounds(), changedDst, image.ZP, finalMask, image.ZP, draw.Over)
+	//apply mask
+	if maskPath != "" && maskDrawPath != "" {
+
+		mask, err := readImage(maskPath)
+		if err != nil {
+			errorAndExit("Error reading mask\n")
+		}
+
+		maskImage, err := readImage(maskDrawPath)
+		if err != nil {
+			errorAndExit("Error reading mask image\n")
+		}
+
+		//Scale mask
+		finalMask := image.NewRGBA(maskImage.Bounds())
+		draw.ApproxBiLinear.Scale(finalMask, maskImage.Bounds(), mask, mask.Bounds(), draw.Over, nil)
+
+		//Draw our image
+		draw.DrawMask(finalDst, finalDst.Bounds(), maskImage, image.ZP, finalMask, image.ZP, draw.Over)
+	}
 
 	//Create output file
 	output, err := os.Create(outputPath)
@@ -77,4 +109,10 @@ func readImage(file string) (image.Image, error) {
 	}
 
 	return image, nil
+}
+
+func errorAndExit(errStr string) {
+	fmt.Println(errStr)
+	flag.Usage()
+	os.Exit(1)
 }
